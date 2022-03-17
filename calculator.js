eeb923fc2e06d22e8a4c2b08de1658aff735ca48
Andrea <andrerogiron16@gmail.com>
2022-03-17 16:55:56 -0600

    avance

---
+++ calculator.js
@@ -0,0 +1,32 @@
+function calculator(cadena) {
+
+    let cadena;
+    let cadenaArray = [cadena];
+
+    for (i == 0; cadena.length; i++) {
+        let enteros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+        if (cadena[i] == enteros) {
+            newArrary = cadena[i]
+
+        }
+
+        if (cadena.length == '*') {
+
+        } else if (cadena.length == '/') {
+
+        } else if (cadena.length == '+') {
+
+        } else if (cadena.length == '-') {
+
+        }
+
+
+    };
+
+
+}
+
+
+
+
+console.log(calculator(cadena))
